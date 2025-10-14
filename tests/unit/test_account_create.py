6a3dab3849ea99a335e800a714b603e8a9500cd1
Artix263 <artix2634@gmail.com>
2025-10-14 15:18:07 +0200

    Feature2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,9 +3,8 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe","123423123")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-    def test_default(self):
-        example_account=Account("Maciej","Dobrzyński")
-        assert example_account.balance==0
+        assert account.pesel=="123423123"
+    
